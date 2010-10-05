8dd34e321fea7fd885025768a7ea98bab83a99e2
Martin Schwab <mschwab@hsr.ch>
2010-10-05 20:11:40 +0200

    Visitor makes first steps.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
@@ -2,6 +2,14 @@ package ch.hsr.eclipse.cdt.ui.toggle;
 
 import java.util.HashMap;
 
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
+import org.eclipse.cdt.core.dom.ast.IASTExpression;
+import org.eclipse.cdt.core.dom.ast.IASTName;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTStatement;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoring;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoringDescription;
@@ -14,6 +22,8 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.core.refactoring.Refactoring;
 import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.text.edits.TextEditGroup;
+import org.eclipse.cdt.ui.CUIPlugin;
 
 import ch.hsr.eclipse.cdt.ui.NullRefactoring;
 
@@ -38,6 +48,39 @@ public class ToggleRefactoring extends CRefactoring {
 	protected void collectModifications(IProgressMonitor pm,
 			ModificationCollector collector) throws CoreException,
 			OperationCanceledException {
+		ASTRewrite rewriter = collector.rewriterForTranslationUnit(unit);
+		ASTVisitor visitor = new ASTVisitor() {
+			{
+				shouldVisitDeclarations = true;
+				shouldVisitNames = true;
+				shouldVisitStatements = true;
+				shouldVisitTranslationUnit = true;
+			}
+			@Override
+			public int visit(IASTDeclaration declaration) {
+				System.out.println("Visit a declaration: " + declaration.getRawSignature());
+				return PROCESS_CONTINUE;
+			}
+			@Override
+			public int visit(IASTName name) {
+				System.out.println("Visit a name: " + name.getRawSignature());
+				return PROCESS_CONTINUE;
+			}
+			@Override
+			public int visit(IASTStatement statement) {
+				System.out.println("Visit a statement: " + statement.getRawSignature());
+				return PROCESS_CONTINUE;
+			}
+			@Override
+			public int visit(IASTTranslationUnit tu) {
+				System.out.println("Tu: " + tu.getRawSignature());
+				return PROCESS_CONTINUE;
+			}
+		};
+		unit.accept(visitor);
+//		IASTNode node = null;
+//		TextEditGroup editGroup = null;
+//		rewriter.remove(node, editGroup);
 	}
 
 }
