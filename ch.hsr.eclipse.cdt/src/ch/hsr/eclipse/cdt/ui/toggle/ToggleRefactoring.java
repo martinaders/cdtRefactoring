251ea9892731fd0b2b3e3e0909a2a219202ece1f
Martin Schwab <mschwab@hsr.ch>
2010-10-06 00:21:14 +0200

    Green bar for the remove member function test.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
@@ -3,15 +3,11 @@ package ch.hsr.eclipse.cdt.ui.toggle;
 import java.util.HashMap;
 
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
-import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
-import org.eclipse.cdt.core.dom.ast.IASTExpression;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
-import org.eclipse.cdt.core.dom.ast.IASTNodeLocation;
-import org.eclipse.cdt.core.dom.ast.IASTStatement;
-import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTSimpleDeclaration;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoring;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoringDescription;
 import org.eclipse.cdt.internal.ui.refactoring.ModificationCollector;
@@ -19,19 +15,11 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.core.refactoring.Refactoring;
 import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
-import org.eclipse.text.edits.DeleteEdit;
-import org.eclipse.text.edits.ReplaceEdit;
-import org.eclipse.text.edits.TextEdit;
 import org.eclipse.text.edits.TextEditGroup;
-import org.eclipse.text.edits.TextEditVisitor;
-import org.eclipse.ui.internal.UIPlugin;
-import org.eclipse.cdt.ui.CUIPlugin;
 
 import ch.hsr.eclipse.cdt.ui.NullRefactoring;
 
@@ -41,13 +29,12 @@ public class ToggleRefactoring extends CRefactoring {
 		{
 			shouldVisitNames = true;
 		}
-		public IASTName member = null;
+		public IASTName memberName = null;
 
 		@Override
 		public int visit(IASTName name) {
-			System.out.println("Visit a name: " + name.getRawSignature());
 			if (name.getRawSignature().equals("member"))
-				member = name;
+				memberName = name;
 			return PROCESS_CONTINUE;
 		}
 	}
@@ -67,18 +54,26 @@ public class ToggleRefactoring extends CRefactoring {
 		};
 	}
 
+	// TODO: use lockIndex, as seen in HideMethodRefactoring
 	@Override
 	protected void collectModifications(IProgressMonitor pm,
 			ModificationCollector collector) throws CoreException,
 			OperationCanceledException {
+		TextEditGroup infoText = new TextEditGroup("Remove member");
+		ASTRewrite rewriter = collector.rewriterForTranslationUnit(unit);
+		rewriter.remove(getMemberDeclaration(), infoText);
+	}
+
+	private IASTNode getMemberDeclaration() {
 		MemberVisitor visitor = new MemberVisitor();
 		unit.accept(visitor);
-		if (visitor.member == null)
-			return;
-		
-		TextEditGroup infoText = new TextEditGroup("Remove " + visitor.member.getRawSignature());
-		ASTRewrite rewriter = collector.rewriterForTranslationUnit(unit);
-		rewriter.remove(visitor.member, infoText);
+		if (isDeclarationFound(visitor))
+			return null;
+		return visitor.memberName.getParent().getParent();
+	}
+
+	private boolean isDeclarationFound(MemberVisitor visitor) {
+		return visitor.memberName == null || visitor.memberName.getParent() == null || !(visitor.memberName.getParent().getParent() instanceof CPPASTSimpleDeclaration);
 	}
 
 }
