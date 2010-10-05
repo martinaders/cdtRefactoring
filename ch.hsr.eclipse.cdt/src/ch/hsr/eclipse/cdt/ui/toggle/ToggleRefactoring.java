d8d3029587f9d5f0e60122994e8a2dfe6112f6a7
Martin Schwab <mschwab@hsr.ch>
2010-10-05 23:23:02 +0200

    Nember name now gets removed. Now it's time to go up in the ASTree and remove the whole declaration.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
@@ -7,6 +7,7 @@ import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTExpression;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTNodeLocation;
 import org.eclipse.cdt.core.dom.ast.IASTStatement;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
@@ -18,16 +19,38 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.core.refactoring.Refactoring;
 import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.text.edits.DeleteEdit;
+import org.eclipse.text.edits.ReplaceEdit;
+import org.eclipse.text.edits.TextEdit;
 import org.eclipse.text.edits.TextEditGroup;
+import org.eclipse.text.edits.TextEditVisitor;
+import org.eclipse.ui.internal.UIPlugin;
 import org.eclipse.cdt.ui.CUIPlugin;
 
 import ch.hsr.eclipse.cdt.ui.NullRefactoring;
 
 public class ToggleRefactoring extends CRefactoring {
+
+	private class MemberVisitor extends ASTVisitor {
+		{
+			shouldVisitNames = true;
+		}
+		public IASTName member = null;
+
+		@Override
+		public int visit(IASTName name) {
+			System.out.println("Visit a name: " + name.getRawSignature());
+			if (name.getRawSignature().equals("member"))
+				member = name;
+			return PROCESS_CONTINUE;
+		}
+	}
 
 	public ToggleRefactoring(IFile file, ISelection selection, ICProject proj) {
 		super(file, selection, null, proj);
@@ -48,39 +71,14 @@ public class ToggleRefactoring extends CRefactoring {
 	protected void collectModifications(IProgressMonitor pm,
 			ModificationCollector collector) throws CoreException,
 			OperationCanceledException {
-		ASTRewrite rewriter = collector.rewriterForTranslationUnit(unit);
-		ASTVisitor visitor = new ASTVisitor() {
-			{
-				shouldVisitDeclarations = true;
-				shouldVisitNames = true;
-				shouldVisitStatements = true;
-				shouldVisitTranslationUnit = true;
-			}
-			@Override
-			public int visit(IASTDeclaration declaration) {
-				System.out.println("Visit a declaration: " + declaration.getRawSignature());
-				return PROCESS_CONTINUE;
-			}
-			@Override
-			public int visit(IASTName name) {
-				System.out.println("Visit a name: " + name.getRawSignature());
-				return PROCESS_CONTINUE;
-			}
-			@Override
-			public int visit(IASTStatement statement) {
-				System.out.println("Visit a statement: " + statement.getRawSignature());
-				return PROCESS_CONTINUE;
-			}
-			@Override
-			public int visit(IASTTranslationUnit tu) {
-				System.out.println("Tu: " + tu.getRawSignature());
-				return PROCESS_CONTINUE;
-			}
-		};
+		MemberVisitor visitor = new MemberVisitor();
 		unit.accept(visitor);
-//		IASTNode node = null;
-//		TextEditGroup editGroup = null;
-//		rewriter.remove(node, editGroup);
+		if (visitor.member == null)
+			return;
+		
+		TextEditGroup infoText = new TextEditGroup("Remove " + visitor.member.getRawSignature());
+		ASTRewrite rewriter = collector.rewriterForTranslationUnit(unit);
+		rewriter.remove(visitor.member, infoText);
 	}
 
 }
