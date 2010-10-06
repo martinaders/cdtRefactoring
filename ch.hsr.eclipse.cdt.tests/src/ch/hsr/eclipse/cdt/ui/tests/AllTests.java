5578549eb79c88887cd800fc439c42a4d130de6b
Martin Schwab <mschwab@hsr.ch>
2010-10-06 19:29:09 +0200

    add NullRefactoringTestSuite to AllTests.

--- ch.hsr.eclipse.cdt.tests/src/ch/hsr/eclipse/cdt/ui/tests/AllTests.java
+++ ch.hsr.eclipse.cdt.tests/src/ch/hsr/eclipse/cdt/ui/tests/AllTests.java
@@ -2,6 +2,7 @@ package ch.hsr.eclipse.cdt.ui.tests;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import ch.hsr.eclipse.cdt.ui.tests.nullrefactoring.NullRefactoringTestSuite;
 import ch.hsr.eclipse.cdt.ui.tests.toggle.ToggleRefactoringTestSuite;
 
 public class AllTests extends TestSuite {
@@ -11,6 +12,7 @@ public class AllTests extends TestSuite {
 	}
 
 	public AllTests() throws Exception {
+		addTest(NullRefactoringTestSuite.suite());
 		addTest(ToggleRefactoringTestSuite.suite());
 	}
 }
