3697a6ab8badac5a3bbb362a7740bbacdc9626b0
Martin Schwab <mschwab@hsr.ch>
2010-10-06 15:25:27 +0200

    code cosmetics in the test suites.

--- ch.hsr.eclipse.cdt.tests/src/ch/hsr/eclipse/cdt/ui/tests/nullrefactoring/NullRefactoringTestSuite.java
+++ ch.hsr.eclipse.cdt.tests/src/ch/hsr/eclipse/cdt/ui/tests/nullrefactoring/NullRefactoringTestSuite.java
@@ -7,7 +7,7 @@ import ch.hsr.eclipse.cdt.ui.tests.ExternalRefactoringTester;
 public class NullRefactoringTestSuite extends TestSuite {
 
 	public static Test suite() throws Exception {
-		TestSuite suite = new TestSuite(NullRefactoringTestSuite.class.getName());
+		TestSuite suite = new NullRefactoringTestSuite();
 		suite.addTest(ExternalRefactoringTester.suite("NullRefactoringTest", "resources/refactoring/NullRefactoring.rts"));
 		return suite;
 	}
