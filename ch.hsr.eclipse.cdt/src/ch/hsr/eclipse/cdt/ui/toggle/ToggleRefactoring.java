b2dbfe27395abc2ef4769f50104db89d77fe71c0
Martin Schwab <mschwab@hsr.ch>
2010-10-18 14:57:23 +0200

    Detection of declaration+definition (when no duplicate names).

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
@@ -4,6 +4,7 @@ import org.eclipse.cdt.core.dom.ast.IASTDeclSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
 import org.eclipse.cdt.core.dom.ast.IASTFunctionDeclarator;
 import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
+import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTSimpleDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTStatement;
 import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
@@ -13,9 +14,12 @@ import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionDefinition;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTSimpleDeclaration;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoring;
 import org.eclipse.cdt.internal.ui.refactoring.ModificationCollector;
+import org.eclipse.cdt.internal.ui.refactoring.utils.SelectionHelper;
+import org.eclipse.core.internal.resources.File;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
 import org.eclipse.text.edits.TextEditGroup;
@@ -23,10 +27,13 @@ import org.eclipse.text.edits.TextEditGroup;
 @SuppressWarnings("restriction")
 public class ToggleRefactoring extends CRefactoring {
 
-	private IASTFunctionDefinition selectedMemberDefinition;
+	private IASTFunctionDefinition selectedDefinition;
+	private CPPASTFunctionDeclarator selectedDeclaration;
+	private final TextSelection selection;
 
 	public ToggleRefactoring(IFile file, ISelection selection, ICProject proj) {
 		super(file, selection, null, proj);
+		this.selection = (TextSelection) selection;
 	}
 
 	@Override
@@ -50,19 +57,64 @@ public class ToggleRefactoring extends CRefactoring {
 	}
 
 	private void collectMoveChanges(ModificationCollector collector) {
-		selectedMemberDefinition = ToggleSelectionHelper.getFirstSelectedFunctionDefinition(region, unit);
-		IASTFunctionDefinition newfunc = getReplacementFunction();
-		addFunctionReplaceModification(collector, newfunc);
+		selectedDeclaration = ToggleSelectionHelper.getSelectedDeclaration( unit, selection);
+		selectedDefinition  = ToggleSelectionHelper.getSelectedDefinition(unit, selection);
+
+//		determinePosition();
+//		IASTFunctionDefinition newfunc = getReplacementFunction();
+//		addFunctionReplaceModification(collector, newfunc);
+	}
+
+	private void determinePosition() {
+		if (selectedDeclaration == null) {
+			System.out.println("No function declaration selected. Cannot toggle. Stopping.");
+			return;
+		}
+		System.out.println("The declarator found: " + selectedDefinition.getRawSignature());
+		System.out.println("The declaration found: " + selectedDeclaration.getRawSignature());
+		if (selectedDefinition.getDeclarator().equals(selectedDeclaration)) {
+			System.out.println("The declarator is the same node as de definition");
+		}
+		if (isSelectionInHeaderFile())
+			System.out.println("We're in a header file.");
+		else
+			System.out.println("We're in a source file.");
+		
+		if (selectedDefinition == null && selectedDeclaration != null) // 2nd part is obsolete
+			System.out.println("We're in a pure deCLARAtion.");
+		else
+			System.out.println("We're in a deFINItion so we can access its body."); // stimmt nicht weils die klassendefinition findet
+
+		if (selectedDefinition.getTranslationUnit().equals(selectedDeclaration.getTranslationUnit()))
+			System.out.println("Declaration and Definition are placed into the same files.");
+		
+//		System.out.println("Declaration and Definition are placed in different files.");
+//		System.out.println("Declaration and Definition are both placed in the header file.");
+//		 System.out.println("and they're together.");
+//		 System.out.println("and they're placed apart.");
+
+//		System.out.println("The specifiers for this declaration: " + selectedDeclaration.getDeclSpecifier());
+//		System.out.println("We're inside a class and the current scope is xy");
+//		System.out.println("We're outside of any class. The function has scope xy. So look there for existing declarations.");
+//		
+//		System.out.println("We've selected a free function.");
+//		 System.out.println("The free function shall be toggled to a separate file.");
+//		 System.out.println("The free function shall be toggled back into the source file.");
+	}
+
+	private boolean isSelectionInHeaderFile() {
+		String filename = unit.getContainingFilename();
+		return filename.endsWith(".h") || filename.endsWith(".hpp");
 	}
 
 	private IASTFunctionDefinition getReplacementFunction() {
-		IASTDeclSpecifier newdeclspec = selectedMemberDefinition.getDeclSpecifier().copy();
+		IASTDeclSpecifier newdeclspec = selectedDefinition.getDeclSpecifier().copy();
 		newdeclspec.setInline(true);
-		IASTFunctionDeclarator funcdecl = new CPPASTFunctionDeclarator(ToggleSelectionHelper.getQualifiedName(selectedMemberDefinition));
+		IASTFunctionDeclarator funcdecl = selectedDeclaration;
 
 		//TODO: add the parameters
 		
-		IASTStatement newbody = selectedMemberDefinition.getBody().copy();
+		IASTStatement newbody = selectedDefinition.getBody().copy();
 		IASTFunctionDefinition newfunc = new CPPASTFunctionDefinition(newdeclspec, funcdecl, newbody);
 		newfunc.setParent(unit);
 		return newfunc;
@@ -72,8 +124,8 @@ public class ToggleRefactoring extends CRefactoring {
 			ModificationCollector collector, IASTFunctionDefinition newfunc) {
 		ASTRewrite rewrite = collector.rewriterForTranslationUnit(unit);
 		TextEditGroup edit = new TextEditGroup("Toggle");
-		IASTSimpleDeclaration declaration = createDeclarationFromDefinition(selectedMemberDefinition);
-		rewrite.replace(selectedMemberDefinition, declaration, edit);
+		IASTSimpleDeclaration declaration = createDeclarationFromDefinition(selectedDefinition);
+		rewrite.replace(selectedDefinition, declaration, edit);
 		rewrite.insertBefore(unit, null, newfunc, edit);
 	}
 
--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleSelectionHelper.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleSelectionHelper.java
@@ -3,6 +3,7 @@ package ch.hsr.eclipse.cdt.ui.toggle;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
 import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
 import org.eclipse.cdt.core.dom.ast.IASTName;
@@ -10,56 +11,63 @@ import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.ast.cpp.CPPASTVisitor;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCompositeTypeSpecifier;
-import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFunctionDefinition;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTQualifiedName;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionDeclarator;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTQualifiedName;
 import org.eclipse.cdt.internal.ui.refactoring.Container;
 import org.eclipse.cdt.internal.ui.refactoring.utils.SelectionHelper;
-import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.TextSelection;
 
 /**
  * Helps finding a FunctionDefinition in the parent nodes of the current selection. 
  */
 class ToggleSelectionHelper extends SelectionHelper {		
-	public static IASTFunctionDefinition getFirstSelectedFunctionDefinition(final Region region, final IASTTranslationUnit unit) {
+
+	public static IASTFunctionDefinition getSelectedDefinition(
+			final IASTTranslationUnit unit, final TextSelection selection) {
 		final Container<IASTFunctionDefinition> container = new Container<IASTFunctionDefinition>();
+		
+		unit.accept(new CPPASTVisitor() {
+			{
+				shouldVisitDeclarations = true;
+			}
+			public int visit(IASTDeclaration node) {
+				if (!(node instanceof IASTFunctionDefinition))
+					return super.visit(node);
+				IASTFunctionDefinition func = (IASTFunctionDefinition) node;
+				String currentNodeName = new String(func.getDeclarator().getName().getSimpleID());
+				if (currentNodeName.equals(selection.getText())) {
+					// TODO: In addition, check if at same offset as selection
+					System.out.println("Found matching definition: " + func.getRawSignature());
+					container.setObject((IASTFunctionDefinition) func);
+				}
+				return super.visit(node);
+			}
+		});
+		return container.getObject();
+	}
+	
+	public static CPPASTFunctionDeclarator getSelectedDeclaration(final IASTTranslationUnit unit, final TextSelection selection) {
+		final Container<CPPASTFunctionDeclarator> container = new Container<CPPASTFunctionDeclarator>();
 		
 		unit.accept(new CPPASTVisitor() {
 			{
 				shouldVisitDeclarators = true;
 			}
-			public int visit(IASTDeclarator declarator) {
-				if (declarator instanceof CPPASTFunctionDeclarator) {
-					if (declarator.getParent() instanceof ICPPASTFunctionDefinition && isSelectionOnExpression(region, unit)) {
-						container.setObject((IASTFunctionDefinition) declarator.getParent());
-					}
+			public int visit(IASTDeclarator node) {
+				if (!(node instanceof CPPASTFunctionDeclarator))
+					return super.visit(node);
+				CPPASTFunctionDeclarator func = (CPPASTFunctionDeclarator) node;
+				String currentNodeName = new String(func.getName().getSimpleID());
+				if (currentNodeName.equals(selection.getText())) {
+					// TODO: In addition, check if at same offset as selection
+					System.out.println("Found matching declaration: " + func.getRawSignature());
+					container.setObject((CPPASTFunctionDeclarator) func);
 				}
-				return super.visit(declarator);
+				return super.visit(node);
 			}
 		});
 		return container.getObject();
 	}
 
-	static ArrayList<IASTName> getAllQualifiedNames(IASTFunctionDefinition memberdefinition) {
-		ArrayList<IASTName> names = new ArrayList<IASTName>();
-		IASTNode node = memberdefinition; 
-		while(node.getParent() != null) {
-			node = node.getParent();
-			if (node instanceof ICPPASTCompositeTypeSpecifier) {
-				names.add(((ICPPASTCompositeTypeSpecifier) node).getName());
-			}
-		}
-		Collections.reverse(names);
-		return names;
-	}
-
-	static ICPPASTQualifiedName getQualifiedName(IASTFunctionDefinition memberdefinition) {
-		ICPPASTQualifiedName newdecl = new CPPASTQualifiedName();
-		for (IASTName name : getAllQualifiedNames(memberdefinition)) {
-			newdecl.addName(name.copy());
-		}
-		newdecl.addName(memberdefinition.getDeclarator().getName().copy());
-		return newdecl;
-	}
 }
