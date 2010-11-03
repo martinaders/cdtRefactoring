17d8f786656cbaa6dfa771151c66d731944de626
Martin Schwab <mschwab@hsr.ch>
2010-11-03 17:42:46 +0100

    solve problem with fuzzy selection.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
@@ -1,35 +1,42 @@
 package ch.hsr.eclipse.cdt.ui.toggle;
 
-import org.eclipse.cdt.core.dom.ast.ASTNameCollector;
-import org.eclipse.cdt.core.dom.ast.DOMException;
+import java.awt.datatransfer.StringSelection;
+import java.io.IOException;
+
+import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
+import org.eclipse.cdt.core.dom.ast.IASTImplicitName;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTSimpleDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
-import org.eclipse.cdt.core.dom.ast.IBinding;
-import org.eclipse.cdt.core.dom.ast.IScope;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFunctionDeclarator;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFunctionDefinition;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTTemplateDeclaration;
-import org.eclipse.cdt.core.index.IIndex;
 import org.eclipse.cdt.core.index.IIndexBinding;
 import org.eclipse.cdt.core.index.IIndexName;
-import org.eclipse.cdt.core.index.IndexFilter;
 import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionDeclarator;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTImplicitName;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoring;
 import org.eclipse.cdt.internal.ui.refactoring.ModificationCollector;
+import org.eclipse.cdt.internal.ui.refactoring.utils.NodeHelper;
+import org.eclipse.cdt.internal.ui.refactoring.utils.SelectionHelper;
+import org.eclipse.cdt.internal.ui.refactoring.utils.TranslationUnitHelper;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.internal.ui.refactoring.util.Strings;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.TextEditor;
 
-import ch.hsr.ifs.redhead.helpers.IndexNameToAstNameMatcher;
 import ch.hsr.ifs.redhead.helpers.IndexToASTNameHelper;
 
 @SuppressWarnings("restriction")
@@ -48,37 +55,44 @@ public class ToggleRefactoring extends CRefactoring {
 	@Override
 	public RefactoringStatus checkInitialConditions(IProgressMonitor pm)
 			throws CoreException, OperationCanceledException {
-		
+
 		unit = ToggleSelectionHelper.getLocalTranslationUnitForFile(file.getLocationURI());
 		if (unit == null) {
 			initStatus.addFatalError("Could not get TranslationUnit for file");
 			return initStatus;
 		}
-		
+
 		IASTName name = unit.getNodeSelector(null).findName(selection.getOffset(), selection.getLength());
+		if (name == null) {
+			name = unit.getNodeSelector(null).findFirstContainedName(selection.getOffset(), selection.getLength());
+			IASTFunctionDefinition fundef = NodeHelper.findFunctionDefinitionInAncestors(name);
+			name = fundef.getDeclarator().getName();
+		}
+		if (name == null) {
+			initStatus.addFatalError("Problems determining the selected function, aborting. Choose another selection.");
+			return initStatus;
+		}
+		System.out.println(name.getClass() + ", " + name.getRawSignature());
 		
 		System.out.println("name was selected: " + name);
-		IIndexName[] decnames = null;
-		IIndexName[] defnames = null;
 		try {
 			lockIndex();
-			IIndex index = getIndex();
 			IIndexBinding binding = getIndex().findBinding(name);
-			decnames = index.findDeclarations(binding);
-			defnames = index.findDefinitions(binding);
-			for(IIndexName iname : decnames) {
-				System.out.println("iname : " + iname.getNodeOffset() + " " + iname.getFile());
+			for(IIndexName iname : getIndex().findDeclarations(binding)) {
+				System.out.println("iname-dec : " + iname.getNodeOffset() + " " + iname.getFile());
 				IASTName astname = IndexToASTNameHelper.findMatchingASTName(unit, iname, getIndex());
 				if (astname != null) {
 					selectedDeclaration = findFunctionDeclarator(astname);
+					System.out.println(" -> " + selectedDeclaration);
 					break;
 				}
 			}
-			for(IIndexName iname : defnames) {
-				System.out.println("iname : " + iname.getNodeOffset() + " " + iname.getFile());
+			for(IIndexName iname : getIndex().findDefinitions(binding)) {
+				System.out.println("iname-def : " + iname.getNodeOffset() + " " + iname.getFile());
 				IASTName astname = IndexToASTNameHelper.findMatchingASTName(unit, iname, getIndex());
 				if (astname != null) {
 					selectedDefinition = findFunctionDefinition(astname);
+					System.out.println(" -> " + selectedDefinition);
 					break;
 				}
 			}
@@ -87,9 +101,9 @@ public class ToggleRefactoring extends CRefactoring {
 		} finally {
 			unlockIndex();
 		}
-		
-		
+
 		if (selectedDeclaration == null || selectedDefinition == null) {
+			System.err.println("fuck");
 			initStatus
 			.addFatalError("declaration AND definition needed. Cannot toggle.");
 			return initStatus;
@@ -112,8 +126,10 @@ public class ToggleRefactoring extends CRefactoring {
 	}
 
 	private IASTFunctionDefinition findFunctionDefinition(IASTNode node) {
+		System.out.print("Analyzing parents: ");
 		while(node.getParent() != null) {
 			node = node.getParent();
+			System.out.print(node.getClass() + ", ");
 			if (node instanceof ICPPASTFunctionDefinition)
 				return (IASTFunctionDefinition) node;
 		}
@@ -121,8 +137,14 @@ public class ToggleRefactoring extends CRefactoring {
 	}
 
 	private CPPASTFunctionDeclarator findFunctionDeclarator(IASTNode node) {
+		System.out.print("Analyzing parents: " + node.getClass() + ", "); //IASTCPPTypeSpecifier
+		if (node instanceof IASTSimpleDeclaration) {
+			System.out.println("\nHell yeah :-)");
+			return (CPPASTFunctionDeclarator)((IASTSimpleDeclaration)node).getDeclarators()[0];
+		}
 		while(node.getParent() != null) {
 			node = node.getParent();
+			System.out.print(node.getClass() + ", ");
 			if (node instanceof ICPPASTFunctionDeclarator)
 				return (CPPASTFunctionDeclarator) node;
 		}
