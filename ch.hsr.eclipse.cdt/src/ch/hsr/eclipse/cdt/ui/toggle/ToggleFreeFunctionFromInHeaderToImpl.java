b943cecf059c6a0585a72fd8a8e112c01f9c95d0
Martin Schwab <mschwab@hsr.ch>
2010-11-07 20:19:54 +0100

    Fix newlines for FreeFunctionInHeaderToImpl.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleFreeFunctionFromInHeaderToImpl.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleFreeFunctionFromInHeaderToImpl.java
@@ -6,7 +6,13 @@ import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.cdt.core.model.CModelException;
 import org.eclipse.cdt.internal.ui.refactoring.ModificationCollector;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ltk.core.refactoring.CompositeChange;
+import org.eclipse.ltk.core.refactoring.TextChange;
+import org.eclipse.text.edits.InsertEdit;
+import org.eclipse.text.edits.ReplaceEdit;
+import org.eclipse.text.edits.TextEdit;
 
+@SuppressWarnings("restriction")
 public class ToggleFreeFunctionFromInHeaderToImpl extends
 		ToggleRefactoringAbstractStrategy {
 
@@ -17,7 +23,6 @@ public class ToggleFreeFunctionFromInHeaderToImpl extends
 		this.siblingfile_translation_unit = context.loadTUForSiblingFile();
 	}
 
-	@SuppressWarnings("restriction")
 	@Override
 	public void run(ModificationCollector modifications) {
 		ASTRewrite astrewriter = modifications.rewriterForTranslationUnit(definition_unit);
@@ -32,4 +37,62 @@ public class ToggleFreeFunctionFromInHeaderToImpl extends
 				selectedDefinition.copy(), infoText);
 	}
 
+	@Override
+	protected void removeNewlines(CompositeChange finalChange) {
+		CompositeChange file1 = (CompositeChange) finalChange.getChildren()[0];
+		CompositeChange file2 = (CompositeChange) finalChange.getChildren()[1];
+		TextEdit edit1 = ((TextChange) file1.getChildren()[0]).getEdit();
+		TextEdit edit2 = ((TextChange) file2.getChildren()[0]).getEdit();
+
+		for (TextEdit e : edit1.getChildren()) {
+			System.out.println("off: " + e.getOffset());
+			System.out.println("len: " + e.getLength());
+			System.out.println("ini: " + e.getInclusiveEnd());
+			System.out.println("ine: " + e.getExclusiveEnd());
+			System.out.println("chi: " + e.getChildrenSize());
+			if (e instanceof ReplaceEdit) {
+				System.out.println("Rep: " + ((ReplaceEdit)e).getText().length() + "---" + ((ReplaceEdit)e).getText() + "---");
+			} else if (e instanceof InsertEdit) {
+				System.out.println("Ins: " + ((InsertEdit)e).getText().length() + "---" + ((InsertEdit)e).getText() + "---");
+			}
+		}
+
+		for (TextEdit e : edit2.getChildren()) {
+			System.out.println("off: " + e.getOffset());
+			System.out.println("len: " + e.getLength());
+			System.out.println("ini: " + e.getInclusiveEnd());
+			System.out.println("ine: " + e.getExclusiveEnd());
+			System.out.println("chi: " + e.getChildrenSize());
+			if (e instanceof ReplaceEdit) {
+				System.out.println("Rep: " + ((ReplaceEdit)e).getText().length() + "---" + ((ReplaceEdit)e).getText() + "---");
+			} else if (e instanceof InsertEdit) {
+				System.out.println("Ins: " + ((InsertEdit)e).getText().length() + "---" + ((InsertEdit)e).getText() + "---");
+			}
+		}
+		
+		boolean variation = edit2.getChildren()[0] instanceof ReplaceEdit;
+
+		ReplaceEdit repEdit = null;
+		InsertEdit insEdit = null;
+		if (variation) {
+			repEdit = (ReplaceEdit) edit2.getChildren()[0];
+			insEdit = (InsertEdit) edit1.getChildren()[0];
+		} else {
+			repEdit = (ReplaceEdit) edit1.getChildren()[0];
+			insEdit = (InsertEdit) edit2.getChildren()[0];
+		}
+
+		insEdit = new InsertEdit(insEdit.getOffset(), insEdit.getText().substring(0, insEdit.getText().length() - 2));
+		repEdit = new ReplaceEdit(repEdit.getOffset(), repEdit.getLength(), repEdit.getText().substring(0, repEdit.getText().length() - 1));
+
+		edit1.removeChild(0);
+		edit2.removeChild(0);
+		if (variation) {
+			edit1.addChild(insEdit);
+			edit2.addChild(repEdit);
+		} else {
+			edit2.addChild(insEdit);
+			edit1.addChild(repEdit);			
+		}
+	}
 }
