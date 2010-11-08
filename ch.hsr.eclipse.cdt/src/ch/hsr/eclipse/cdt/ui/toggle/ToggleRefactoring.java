e74f11b3d09b2a15aad0edd26543ef750b64a56e
Martin Schwab <mschwab@hsr.ch>
2010-11-08 15:02:53 +0100

    Remove System.out's.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
@@ -26,7 +26,6 @@ public class ToggleRefactoring extends CRefactoring {
 	public ToggleRefactoring(IFile file, ISelection selection, ICProject proj) {
 		super(file, selection, null, proj);
 		if (selection == null || file == null || project == null) {
-			System.err.println("no selection found");
 			initStatus.addFatalError("Preconditions for this refactoring not fulfilled, aborting.");
 			return;
 		}
