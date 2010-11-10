e127d4a2e4b7fb5ede8767f8338f59a759864825
Martin Schwab <mschwab@hsr.ch>
2010-11-10 21:12:18 +0100

    add fault tol stuff.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoring.java
@@ -1,5 +1,7 @@
 package ch.hsr.eclipse.cdt.ui.toggle;
 
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.index.IIndexManager;
 import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.cdt.internal.ui.editor.CEditor;
 import org.eclipse.cdt.internal.ui.refactoring.CRefactoring;
@@ -11,14 +13,19 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jface.text.TextSelection;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ltk.core.refactoring.RefactoringDescriptor;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.IDE;
 
+/**
+ * Determines whether a valid function was selected by the user to be able to
+ * run the appropriate strategy for moving the function body to another
+ * position.
+ */
 @SuppressWarnings("restriction")
 public class ToggleRefactoring extends CRefactoring {
 
@@ -26,13 +33,13 @@ public class ToggleRefactoring extends CRefactoring {
 	private ToggleRefactoringAbstractStrategy strategy;
 	private ToggleRefactoringContext context;
 	
-	public ToggleRefactoring(IFile file, ISelection selection, ICProject proj) {
+	public ToggleRefactoring(IFile file, TextSelection selection, ICProject proj) {
 		super(file, selection, null, proj);
 		if (selection == null || file == null || project == null) {
 			initStatus.addFatalError("Preconditions for this refactoring not fulfilled, aborting.");
 			return;
 		}
-		this.selection = (TextSelection) selection;
+		this.selection = selection;
 		IDE.saveAllEditors(new IResource[] {ResourcesPlugin.getWorkspace().getRoot()}, false);
 	}
 
@@ -40,13 +47,11 @@ public class ToggleRefactoring extends CRefactoring {
 	public RefactoringStatus checkInitialConditions(IProgressMonitor pm)
 			throws CoreException, OperationCanceledException {
 		try {
+			waitForIndexer();
+			if (initStatus.hasFatalError())
+				return initStatus;
 			lockIndex();
-			context = new ToggleRefactoringContext(getIndex());
-			context.findFileUnitTranslation(file, initStatus);
-			context.findASTNodeName(selection, initStatus);
-			context.findBinding(initStatus);
-			context.findDeclaration(initStatus);
-			context.findDefinition(initStatus);
+			initContext();
 		} catch (InterruptedException e) {
 		} finally {
 			unlockIndex();
@@ -59,28 +64,28 @@ public class ToggleRefactoring extends CRefactoring {
 		return initStatus;
 	}
 
-	@Override
-	protected void collectModifications(IProgressMonitor pm,
-			ModificationCollector modifications) throws CoreException {
-		try {
-			lockIndex();
-			try {
-				strategy.run(modifications);
-			} finally {
-				unlockIndex();
-			}
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		} catch (IllegalArgumentException e) {
-			CUIPlugin.log("Failed to commit changes. Building project may help.", e);
-		}
+	private void initContext() {
+		context = new ToggleRefactoringContext(getIndex());
+		context.findFileUnitTranslation(file, initStatus);
+		context.findASTNodeName(selection, initStatus);
+		context.findBinding(initStatus);
+		context.findDeclaration(initStatus);
+		context.findDefinition(initStatus);
+	}
+
+	private void waitForIndexer() {
+		final IIndexManager im = CCorePlugin.getIndexManager();
+		System.out.println("before Join");
+		im.joinIndexer(1500, new NullProgressMonitor());
+		if (!im.isProjectIndexed(project))
+			initStatus.addFatalError("Indexer not yet finished. Be patient and then try again.");
+		System.out.println("after Join");
 	}
 
 	@Override
-	public RefactoringStatus checkFinalConditions(IProgressMonitor pm)
-			throws CoreException, OperationCanceledException {
-		return super.checkFinalConditions(pm);
-		
+	protected void collectModifications(IProgressMonitor pm,
+			ModificationCollector modifications) throws CoreException {
+		strategy.run(modifications);
 	}
 
 	@Override
@@ -88,6 +93,11 @@ public class ToggleRefactoring extends CRefactoring {
 		return new EmptyRefactoringDescription();
 	}
 
+	/**
+	 * Some strategies create a new file which should be shown to the user. The
+	 * TogglingActionDelegate invokes this method after having applied the
+	 * collected changes and the new file has been created.
+	 */
 	public void openEditorIfNeeded() {
 		if (strategy.shouldOpenFile == null)
 			return;
