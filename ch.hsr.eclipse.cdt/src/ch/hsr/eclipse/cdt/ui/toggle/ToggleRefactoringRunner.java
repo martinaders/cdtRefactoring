fcb7f0f435398aa496737ad955e8502cab04c9aa
Martin Schwab <mschwab@hsr.ch>
2010-11-17 18:41:33 +0100

    temp: concurrency issues

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoringRunner.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleRefactoringRunner.java
@@ -1,5 +1,7 @@
 package ch.hsr.eclipse.cdt.ui.toggle;
 
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.index.IIndexManager;
 import org.eclipse.cdt.core.model.ICElement;
 import org.eclipse.cdt.core.model.ICProject;
 import org.eclipse.cdt.internal.ui.refactoring.RefactoringRunner;
@@ -8,6 +10,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.window.IShellProvider;
@@ -44,12 +47,20 @@ public class ToggleRefactoringRunner extends RefactoringRunner {
 				if (status.hasFatalError())
 					return JobStatus.CANCEL_STATUS;
 				change = refactoring.createChange(monitor);
+				change.initializeValidationData(monitor);
+				if (!change.isValid(monitor).isOK()) {
+					System.err.println("Nah, not valid :-(");
+					return JobStatus.CANCEL_STATUS;
+				}
 				System.err.print("Locking: ");
 				undoManager.aboutToPerformChange(change);
 				System.err.println("OK");
 				
 				undoChange = change.perform(monitor);
 				success = true;
+				IIndexManager im = CCorePlugin.getIndexManager();
+				im.reindex(project);
+				im.joinIndexer(IIndexManager.FOREVER, monitor);
 			} catch (IllegalStateException e) {
 				System.err.println("Another refactoring is still in progress, aborting.");
 			} catch (CoreException e) {
@@ -57,12 +68,16 @@ public class ToggleRefactoringRunner extends RefactoringRunner {
 				e.printStackTrace();
 			} finally {
 				System.err.print("Unlocking: ");
+				undoChange.initializeValidationData(monitor);
 				undoManager.changePerformed(change, success);					
-				if (success) {
-					undoChange.initializeValidationData(monitor);
-					// Note: addUndo MUST be called AFTER changePerformed or
-					// the change won't be unlocked correctly. (17.11.2010)
-					undoManager.addUndo("toggle function body", undoChange);
+				try {
+					if (success && undoChange.getAffectedObjects().length == 0) {
+						// Note: addUndo MUST be called AFTER changePerformed or
+						// the change won't be unlocked correctly. (17.11.2010)
+						undoManager.addUndo("toggle function body", undoChange);
+					}
+				} catch (OperationCanceledException e) {
+					e.printStackTrace();
 				}
 				System.err.println(" OK");
 			}
@@ -80,5 +95,4 @@ public class ToggleRefactoringRunner extends RefactoringRunner {
 	public void run() {
 		new RefactoringJob().schedule();
 	}
-
 }
