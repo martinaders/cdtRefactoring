aab6251db45df0894e5bde15444c20e068759bfa
Martin Schwab <mschwab@hsr.ch>
2010-11-18 22:39:11 +0100

    add comment handlig and selection testsuite.

--- ch.hsr.eclipse.cdt.tests/src/ch/hsr/eclipse/cdt/ui/tests/toggle/ToggleRefactoringTestSuite.java
+++ ch.hsr.eclipse.cdt.tests/src/ch/hsr/eclipse/cdt/ui/tests/toggle/ToggleRefactoringTestSuite.java
@@ -8,6 +8,8 @@ public class ToggleRefactoringTestSuite extends TestSuite {
 
 	public static Test suite() throws Exception {
 		TestSuite suite = new ToggleRefactoringTestSuite();
+		suite.addTest(ExternalRefactoringTester.suite("ToggleCommentsTest", "resources/refactoring/ToggleComments.rts"));
+		suite.addTest(ExternalRefactoringTester.suite("ToggleSelectionTest", "resources/refactoring/ToggleSelection.rts"));
 		suite.addTest(ExternalRefactoringTester.suite("ToggleSimpleFunctionRefactoringTest", "resources/refactoring/ToggleSimpleFunctionRefactoring.rts"));
 		suite.addTest(ExternalRefactoringTester.suite("ToggleTemplateRefactoringTest", "resources/refactoring/ToggleTemplateRefactoring.rts"));
 		suite.addTest(ExternalRefactoringTester.suite("ToggleNamespaceRefacotringTest", "resources/refactoring/ToggleNamespaceRefactoring.rts"));
