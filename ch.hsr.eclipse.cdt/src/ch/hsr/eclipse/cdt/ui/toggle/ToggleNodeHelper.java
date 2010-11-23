278fe39e18de3b18bff3f60758c7451fd2e4a001
Thomas Kallenberg <thomas@no-more-secrets.ch>
2010-11-23 13:23:57 +0100

    Added copyright on each source file

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleNodeHelper.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleNodeHelper.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Institute for Software, HSR Hochschule fuer Technik  
+ * Rapperswil, University of applied sciences and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html  
+ * 
+ * Contributors: 
+ * 		Martin Schwab & Thomas Kallenberg - initial API and implementation 
+ ******************************************************************************/
 package ch.hsr.eclipse.cdt.ui.toggle;
 
 import java.net.URI;
