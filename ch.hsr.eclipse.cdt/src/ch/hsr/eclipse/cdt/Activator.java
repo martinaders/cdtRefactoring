acf57c8d93931d7871a30bc4a9483e036a6a50d6
Martin Schwab <mschwab@hsr.ch>
2010-12-05 02:41:36 +0100

    Doc: tried to separate introduction and specification clearly.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/Activator.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/Activator.java
@@ -9,23 +9,12 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	public static final String PLUGIN_ID = "ch.hsr.eclipse.cdt"; //$NON-NLS-1$
-	private static Activator plugin;
-	
-	public Activator() {
-	}
 
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		plugin = this;
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
 		super.stop(context);
 	}
-
-	public static Activator getDefault() {
-		return plugin;
-	}
-
 }
--- ch.hsr.eclipse.doc/src/main/latex/project-documentation/outside_class_implementation.h
+++ ch.hsr.eclipse.doc/src/main/latex/project-documentation/outside_class_implementation.h
@@ -4,7 +4,7 @@ class A {
 	int x();
 };
  
-int A::x() { 
+inline int A::x() { 
 	/* implementation */ 
 }
 #endif
