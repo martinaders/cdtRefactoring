b68613de1ee0290bb86bb834f2b4c48542c9e2a9
Martin Schwab <mschwab@hsr.ch>
2010-12-07 14:51:59 +0100

    Add support for InHeaderToClass Comments.

--- ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleFromInHeaderToClassStrategy.java
+++ ch.hsr.eclipse.cdt/src/ch/hsr/eclipse/cdt/ui/toggle/ToggleFromInHeaderToClassStrategy.java
@@ -11,7 +11,18 @@
  ******************************************************************************/
 package ch.hsr.eclipse.cdt.ui.toggle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.eclipse.cdt.core.dom.ast.IASTComment;
+import org.eclipse.cdt.core.dom.ast.IASTFunctionDeclarator;
+import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTCatchHandler;
 import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionWithTryBlock;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTSimpleDeclaration;
+import org.eclipse.cdt.internal.core.dom.rewrite.ASTLiteralNode;
+import org.eclipse.cdt.internal.core.dom.rewrite.commenthandler.ASTCommenter;
 import org.eclipse.cdt.internal.ui.refactoring.ModificationCollector;
 import org.eclipse.text.edits.TextEditGroup;
 
@@ -21,10 +32,21 @@ public class ToggleFromInHeaderToClassStrategy implements
 
 	private TextEditGroup infoText;
 	private ToggleRefactoringContext context;
+	private CPPASTSimpleDeclaration declaration;
 
 	public ToggleFromInHeaderToClassStrategy(ToggleRefactoringContext context) {
 		this.context = context;
 		this.infoText =  new TextEditGroup("Toggle function body placement");
+		declaration = getParentSimpleDeclaration(context.getDeclaration());
+		if (declaration == null) {
+			throw new NotSupportedException("Parent needs to be a SimpleDeclaration.");
+		}
+	}
+
+	private CPPASTSimpleDeclaration getParentSimpleDeclaration(IASTFunctionDeclarator node) {
+		if (node == null || node.getParent() == null || !(node.getParent() instanceof CPPASTSimpleDeclaration))
+			return null;
+		return (CPPASTSimpleDeclaration) node.getParent();
 	}
 
 	@Override
@@ -32,9 +54,36 @@ public class ToggleFromInHeaderToClassStrategy implements
 		ASTRewrite rewriter = modifications.rewriterForTranslationUnit(context
 				.getDefinitionUnit());
 		rewriter.remove(ToggleNodeHelper.getParentRemovePoint(context.getDefinition()), infoText);
-		rewriter.replace(context.getDeclaration().getParent(),
-				ToggleNodeHelper.createInClassDefinition( context.getDeclaration(), 
-						context.getDefinition(), context.getDefinitionUnit()), infoText);
+		IASTFunctionDefinition newDefinition = ToggleNodeHelper.createInClassDefinition( context.getDeclaration(), 
+				context.getDefinition(), context.getDefinitionUnit());
+		rewriter.replace(context.getDeclaration().getParent(), newDefinition, infoText);
+		rewriter.replace(newDefinition.getBody(), new ASTLiteralNode(format(context.getDefinition().getBody().getRawSignature())), infoText);
+		rewriteCatchHandlers(rewriter, newDefinition);
 
+		String declarationString = format(addLeadingComments(declaration));
+		rewriter.replace(newDefinition.getDeclSpecifier(), new ASTLiteralNode(declarationString), infoText);
+	}
+
+	private String addLeadingComments(CPPASTSimpleDeclaration decl) {
+		String code = decl.getDeclSpecifier().toString();
+		ArrayList<IASTComment> leadingComments = ASTCommenter.getCommentedNodeMap(context.getDeclarationUnit()).getLeadingCommentsForNode(decl);
+		Collections.reverse(leadingComments);
+		for (IASTComment c : leadingComments)
+			code = c.getRawSignature() + "\n    " + code;
+		return code;
+	}
+
+	private void rewriteCatchHandlers(ASTRewrite rewriter, IASTFunctionDefinition newDefinition) {
+		if (newDefinition instanceof CPPASTFunctionWithTryBlock) {
+			ICPPASTCatchHandler[] newCatches = ((CPPASTFunctionWithTryBlock)newDefinition).getCatchHandlers();
+			ICPPASTCatchHandler[] oldCatches = ((CPPASTFunctionWithTryBlock)context.getDefinition()).getCatchHandlers();
+			for (int i = 0; i < oldCatches.length; i++)
+				rewriter.replace(newCatches[i], new ASTLiteralNode(" " + format(oldCatches[i].getRawSignature())), infoText);
+		}
+	}
+
+	private String format(String rawSignature) {
+		String result = rawSignature.replace("\n", "\n    ");
+		return result;
 	}
 }
